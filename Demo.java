a29e2fcc574cec1f48b8613c104e3b5e7e96048c
Jibin Babu <jibinbab@buffalo.edu>
2017-06-01 11:47:21 -0400

    added Demo.java

---
+++ Demo.java
@@ -0,0 +1,19 @@
+class Jenkins {
+	public Jenkins(){
+		
+	}
+	public void display() {
+		
+		System.out.println("This is a sample program");
+	}
+	
+}
+public class Demo {
+
+	public static void main(String[] args) {
+		Jenkins j= new Jenkins();
+		
+j.display();
+	}
+
+}
